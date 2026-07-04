04ccc7559dc73c6e3af66e972bcf90e1020264e3
Amrulzam <amrulzameer@gmail.com>
2026-07-05 00:45:39 +0530

    chore: set base path to /my-portfolio/ and improve babel plugin formatting in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/my-portfolio/",
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({
+      presets: [reactCompilerPreset()]
+    })
   ],
 })
